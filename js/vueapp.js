0b0bbd935f8d478e766aaf6a06ea850914021e6a
Nikhil Ramanuja <Nikhil.ramanuja.8@gmail.com>
2024-02-10 14:35:40 -0800

    .

--- js/vueapp.js
+++ js/vueapp.js
@@ -11,36 +11,60 @@ export const app = {
 
   methods: {
     async updateSortedTeamList() {
+      
       let eventCode = this.eventCode;
       if (eventCode === "") {
         this.message = "Please enter an event code";
         return;
       }
+      setCookie("eventCode", this.eventCode, 3);
       this.message = "loading...";
 
       // Now you have an event code
       // fetch the data from the server
+      if(!eventCode.startsWith("RE-VRC-")){
+        this.message = "invalid event code";
+        return;
+      }
+      var eventID = await this.getREDeventID(eventCode);
+      if(eventID == null){
+        this.message = "invalid event code";
+        return;
+      }
+      console.log("Event ID: " + eventID);
       console.log("Fetching data for event code: " + eventCode);
-      var teamList = await this.getTeamsFromEvent(eventCode);
-      var matchList = await this.getMatchesFromEvent(eventCode);
-      var currMatch = await this.getCurrMatchFromEvent(eventCode);
-      this.message = "Data loaded";
+      var [teamList, matchList, currMatch] = await Promise.all([
+        this.getTeamsFromEvent(eventID),
+        this.getMatchesFromEvent(eventID),
+        this.getCurrMatchFromEvent(eventID)
+      ]);
+      
+      if(teamList == null){
+        this.message = "invalid event code";
+        return;
+      }
+      if(matchList == null){
+        this.message = "Match schedule not released yet"
+        return;
+      }
+      this.message = "Data loaded ";
       //currMatch = 5;
       // sort it
       console.log("Sorting data for event code: " + eventCode);
       console.log("Match List: " + matchList);
       console.log("Team List: " + teamList);
       console.log("Current Match: " + currMatch);
-
+      this.message = "Current Match: " + currMatch;
       var sortedList = this.generateSortedList(
         matchList,
         teamList,
         currMatch
       );
       // and fill this array
+      
       console.log("Sorted List: " + sortedList);
       this.sortedTeamList = sortedList;
-      this.message = "";
+      
     },
     async getREData(url) {
       var headers = {
@@ -54,20 +78,23 @@ export const app = {
       var data = await response.json();
       return data;
     },
-    async getTeamsFromEvent(reCode) {
-      //  Turn the code into an event ID.
+    async getREDeventID(reCode) {
       var eventcode = await this.getREData(
         "https://www.robotevents.com/api/v2/events?sku%5B%5D=" +
         reCode +
         "&myEvents=false"
       );
-      console.log(eventcode);
+      if(eventcode["data"][0] == null){
+        return null;
+      }
       eventcode = parseInt(eventcode["data"][0]["id"]);
-
+      return eventcode;
+    },
+    async getTeamsFromEvent(eventID) {
       //  Get the teams from the event.
       var eventTeams = await this.getREData(
         "https://www.robotevents.com/api/v2/events/" +
-        eventcode +
+        eventID +
         "/teams?myTeams=false&per_page=250"
       );
       eventTeams = eventTeams["data"];
@@ -78,24 +105,24 @@ export const app = {
       }
       return teamList;
     },
-    async getMatchesFromEvent(reCode) {
+    async getMatchesFromEvent(eventID) {
       //  Turn the code into an event ID.
-      var eventcode = await this.getREData(
-        "https://www.robotevents.com/api/v2/events?sku%5B%5D=" +
-        reCode +
-        "&myEvents=false"
-      );
-      eventcode = parseInt(eventcode["data"][0]["id"]);
+      
       //  Get the teams from the event.
 
       var eventTeams = await this.getREData(
         "https://www.robotevents.com/api/v2/events/" +
-        eventcode +
+        eventID +
         "/divisions/1/matches?per_page=250"
       );
       var matchesData = eventTeams["data"];
 
       var matchList = [];
+      if(matchesData.length == 0 || matchesData == null){
+        return null;
+      }
+      console.log("Matches Data: " + matchesData);
+      console.log("Matches Data Length: " + matchesData.length);
       for (var i = 0; i < matchesData.length; i++) {
         var match = matchesData[i];
 
@@ -124,26 +151,18 @@ export const app = {
 
       return matchList;
     },
-    async getCurrMatchFromEvent(reCode) {
-      //  Turn the code into an event ID.
-      var eventcode = await this.getREData(
-        "https://www.robotevents.com/api/v2/events?sku%5B%5D=" +
-        reCode +
-        "&myEvents=false"
-      );
-      eventcode = parseInt(eventcode["data"][0]["id"]);
-      //  Get the teams from the event.
-
+    async getCurrMatchFromEvent(eventID) {
+      //  Turn the code into an event ID
       var eventTeams = await this.getREData(
         "https://www.robotevents.com/api/v2/events/" +
-        eventcode +
+        eventID +
         "/divisions/1/matches?per_page=250"
       );
       var matchesData = eventTeams["data"];
       for (var i = 0; i < matchesData.length; i++) {
         var match = matchesData[i];
         // Creating an array for the current match
-        if (match["started"] == null) {
+        if (match["started"] == null && matchesData[i+1]["started"] == null) {
           break;
         }
       }
@@ -185,7 +204,7 @@ export const app = {
           break;
         }
         let val = this.findRowNumber(teams[i], matchList, inital);
-        output.push(val -inital +1);
+        output.push(val - inital + 1);
       }
       return output;
     },
@@ -209,7 +228,70 @@ export const app = {
       }
       return i;
     },
+    updateFunction : function(){
+      console.log("updateFunction");
+      let eventCode = this.eventCode;
+      if(eventCode === ""){
+        return;
+      }
+      if(!eventCode.startsWith("RE-VRC-")){
+        return;
+      }
+      if(eventCode.length != 14){
+        return;
+      }
+      this.updateSortedTeamList();
+      console.log("e");
+      
+    },
   },
+  mounted() {
+    const searchParams = new URLSearchParams(window.location.search);
+    console.log("mounted");
+    this.interval = setInterval(() => this.updateFunction(), 60000);
+    if (searchParams.has("e")) {
+      console.log("Event code found in URL");
+      this.eventCode = searchParams.get("e");
+      this.updateSortedTeamList();
+      setCookie("eventCode", this.eventCode, 3);
+    }else{
+      console.log("Event code not found in URL");
+      this.eventCode = getCookie("eventCode");
+      if(this.eventCode != ""){
+        this.updateSortedTeamList();
+      }
+    }
+    
+  },
+  beforeDestroy(){
+    clearInterval(this.interval);
+  }
+
+
 };
 
+function setCookie(cname, cvalue, exdays) {
+  console.log("Setting cookie")
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  console.log("Getting cookie")
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 Vue.createApp(app).mount("#app");

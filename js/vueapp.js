b5cd1cc6c243f347b2edfcc75bf071687eefecf7
Nikhil Ramanuja <Nikhil.ramanuja.8@gmail.com>
2024-09-29 21:17:18 -0700

    .

--- js/vueapp.js
+++ js/vueapp.js
@@ -5,32 +5,106 @@ export const app = {
     return {
       eventCode: "",
       message: "",
-      sortedTeamList: [],
+      eventID: Number,
+      sortedTeamList: [{ number: "3050X", time: 0, selected: false }],
+      displayTeamList: [],
+      selectedTeamList: [],
+      reverse: false,
+      showSelected: false,
+      teamInput: "",
+      currMatch: 0,
+      teamList: [],
+      matchList: [],
     };
   },
 
   methods: {
-    async updateSortedTeamList() {
-      
+    addTeamButton: function () {
+      //funciton called when add team button is clicked
+      if (this.teamInput) {
+        this.selectedTeamList.push(this.teamInput);
+        let team = this.teamInput;
+        this.teamInput = '';
+        console.log(this.selectedTeamList);
+        setCookie('selectedTeamList', JSON.stringify(this.selectedTeamList), 3);
+        for (var i = 0; i < this.sortedTeamList.length; i++) {
+          if (team.toLowerCase() === this.sortedTeamList[i].number.toLowerCase() || this.sortedTeamList[i].number.startsWith(team.toLowerCase())) {
+            console.log("found team")
+            this.sortedTeamList[i].selected = true;
+          }
+        }
+        //this.updateSortedTeamList();
+      }
+    },
+    //function called when a team is checked or unchecked
+    addTeam: function (team) {
+      if (team.selected == true) {
+        this.selectedTeamList.push(team.number);
+        console.log(this.selectedTeamList);
+
+      } else {
+        //remove team
+        console.log("uncheck")
+        for (var i = 0; i < this.selectedTeamList.length; i++) {
+          if (team.number.toLowerCase() === this.selectedTeamList[i].toLowerCase() || this.selectedTeamList[i].startsWith(team.number.toLowerCase())) {
+            this.selectedTeamList.splice(i, 1);
+          }
+        }
+      }
+      setCookie('selectedTeamList', JSON.stringify(this.selectedTeamList), 3);
+    },
+    removeTeam: function (index) {
+      var teamNumber = this.selectedTeamList[index];
+      this.selectedTeamList.splice(index, 1);
+      for (var i = 0; i < this.sortedTeamList.length; i++) {
+        if (teamNumber.toLowerCase() === this.sortedTeamList[i].number.toLowerCase() || this.sortedTeamList[i].number.startsWith(teamNumber.toLowerCase())) {
+          this.sortedTeamList[i].selected = false;
+
+          if (this.showSelected) {
+            console.log("removing team from display list");
+            var team = this.sortedTeamList[i];
+            let index = this.displayTeamList.indexOf(team);
+            this.displayTeamList.splice(index, 1);
+          }
+        }
+      }
+      setCookie('selectedTeamList', JSON.stringify(this.selectedTeamList), 3);
+
+    },
+    async updateAll() {
+      await this.updateEventData();
+      await this.updateSortedTeamList();
+    },
+    //function called to refresh data from RE
+    async updateEventData() {
       let eventCode = this.eventCode;
+
       if (eventCode === "") {
         this.message = "Please enter an event code";
         return;
       }
-      setCookie("eventCode", this.eventCode, 3);
+      var eventID;
+      if (getCookie("eventID") == null) {
+        eventID = await this.getREDeventID(eventCode);
+        setCookie("eventID", eventID, 3);
+      }
+      else if (getCookie("eventCode") == this.eventCode) {
+        eventID = getCookie("eventID");
+        console.log("getting cookie")
+      } else {
+        setCookie("eventCode", this.eventCode, 3);
+        eventID = await this.getREDeventID(eventCode);
+        setCookie("eventID", eventID, 3);
+        console.log("setting cookie");
+      }
       this.message = "loading...";
 
       // Now you have an event code
       // fetch the data from the server
-      if(!eventCode.startsWith("RE-VRC-")){
-        this.message = "invalid event code";
-        return;
-      }
-      var eventID = await this.getREDeventID(eventCode);
-      if(eventID == null){
-        this.message = "invalid event code";
-        return;
-      }
+
+
+
+      if (eventID == null) { this.message = "invalid event code"; return; }
       console.log("Event ID: " + eventID);
       console.log("Fetching data for event code: " + eventCode);
       var [teamList, matchList, currMatch] = await Promise.all([
@@ -38,33 +112,100 @@ export const app = {
         this.getMatchesFromEvent(eventID),
         this.getCurrMatchFromEvent(eventID)
       ]);
-      
-      if(teamList == null){
+      console.log("Team List: " + teamList + "eeee");
+      this.currMatch = currMatch;
+      for (let i = 0; i < teamList.length; i++) {
+
+        teamList[i] = { number: teamList[i], time: 0, selected: false };
+        if (this.sortedTeamList != null && this.sortedTeamList.length > 0) {
+          //loop through sortedTeamList to se see if the current team from teamList exists
+          for (let j = 0; j < teamList.length; j++) {
+            if (teamList[i].number.toLowerCase() == this.sortedTeamList[j].number.toLowerCase()) {
+              teamList[i].selected = this.sortedTeamList[j].selected;
+              break;
+            }
+          }
+        }
+      }
+      this.matchList = matchList;
+      this.sortedTeamList = teamList;
+      if (teamList == null) {
         this.message = "invalid event code";
         return;
       }
-      if(matchList == null){
+      if (matchList == null) {
         this.message = "Match schedule not released yet"
+        this.matchList = [
+          ["1234A", "1234B", "1234C", "1234D"],
+          ["1234E", "1234F", "1234G", "1234H"],
+        ];
+        this.currMatch = 0;
+        // for(var i = 0; i < teamList.length; i++){
+        //   this.sortedTeamList[i] = {number:teamList[i].number, time: 0, selected: false};
+        // }
         return;
       }
       this.message = "Data loaded ";
       //currMatch = 5;
       // sort it
-      console.log("Sorting data for event code: " + eventCode);
-      console.log("Match List: " + matchList);
-      console.log("Team List: " + teamList);
-      console.log("Current Match: " + currMatch);
+      //console.log("Sorting data for event code: " + eventCode);
+      //console.log("Match List: " + matchList);
+      //console.log("Team List: " + teamList);
+      //console.log("Current Match: " + currMatch);
       this.message = "Current Match: " + currMatch;
-      var sortedList = this.generateSortedList(
-        matchList,
-        teamList,
-        currMatch
-      );
+    },
+
+    //function called to update the sorted team list
+    async updateSortedTeamList() {
+
+      //console.log("updateSortedTeamList: " + this.sortedTeamList);
+      // if(this.showSelected==false){
+      //   await this.updateEventData();
+      // }
+      console.log(this.matchList)
+      var sortedList = this.generateSortedList(this.matchList, this.sortedTeamList, this.currMatch);
+      console.log("finished generation")
+      console.log(this.sortedTeamList.length)
+      for (let i = 0; i < this.sortedTeamList.length; i++) {
+
+        if (this.sortedTeamList[i].selected) {
+          console.log("selected team detected")
+          var index = -1;
+          for (let j = 0; j < sortedList.length; j++) {
+            if (sortedList[j].number == this.sortedTeamList[i].number) {
+              index = j;
+              break;
+            }
+          }
+          if (index != -1) {
+            sortedList[i].selected = true;
+          }
+        }
+      }
+      /*
+      auohaepnaofiauebuopabv ipeip359 2125, 2151;
+       {} eflaa faep acaemf;h83 2 35325
+      */
+      for(let i =0;i<this.selectedTeamList.length;i++){
+        for (let j = 0; j < sortedList.length; j++) {
+          let teamNum = this.selectedTeamList[i];
+          console.log("teamnum" + teamNum)
+          if (teamNum == sortedList[j].number || sortedList[j].number.startsWith(teamNum) ) {
+            console.log("found it")
+            sortedList[j].selected = true
+          }
+        }
+      }
       // and fill this array
-      
-      console.log("Sorted List: " + sortedList);
+      if (this.reverse) {
+        sortedList = sortedList.reverse();
+      }
+      //console.log(this.showSelected)
       this.sortedTeamList = sortedList;
-      
+      if (this.selectedTeamList.length > 0 && this.showSelected) {
+        sortedList = sortedList.filter(team => team.selected === true);
+      }
+      this.displayTeamList = sortedList;
     },
     async getREData(url) {
       var headers = {
@@ -84,7 +225,7 @@ export const app = {
         reCode +
         "&myEvents=false"
       );
-      if(eventcode["data"][0] == null){
+      if (eventcode["data"][0] == null) {
         return null;
       }
       eventcode = parseInt(eventcode["data"][0]["id"]);
@@ -107,7 +248,7 @@ export const app = {
     },
     async getMatchesFromEvent(eventID) {
       //  Turn the code into an event ID.
-      
+
       //  Get the teams from the event.
 
       var eventTeams = await this.getREData(
@@ -118,7 +259,7 @@ export const app = {
       var matchesData = eventTeams["data"];
 
       var matchList = [];
-      if(matchesData.length == 0 || matchesData == null){
+      if (matchesData.length == 0 || matchesData == null) {
         return null;
       }
       console.log("Matches Data: " + matchesData);
@@ -162,7 +303,7 @@ export const app = {
       for (var i = 0; i < matchesData.length; i++) {
         var match = matchesData[i];
         // Creating an array for the current match
-        if (match["started"] == null && matchesData[i+1]["started"] == null) {
+        if (match["started"] == null && matchesData[i + 1]["started"] == null) {
           break;
         }
       }
@@ -171,50 +312,49 @@ export const app = {
     generateSortedList: function (
       matchList,
       teamlist,
-      inital
+      inital,
     ) {
-      console.log("generateSortedList");
-      var output = this.generateList(
+      //console.log("generateSortedListz; " + teamlist);
+      var teams = this.generateList(
         matchList,
         teamlist,
         inital
       );
 
-      var indices = Array.from(output.keys());
-
+      //console.log("generateSortedList: " + teams);
       // Sort the indices based on corresponding values in output in descending order
-      indices.sort(function (a, b) {
-        return output[b] - output[a];
+      teams.sort(function (a, b) {
+        return b.time - a.time;
       });
 
-      // Map the sorted indices to corresponding teams in teamlist
-      var finalOutput = indices.map(function (index) {
-        return [teamlist[index], output[index]];
-      });
-
-      return finalOutput;
+      return teams;
     },
     generateList: function (matchList, teamlist, inital) {
-      console.log("generateList");
+      //console.log("generateList");
       var teams = teamlist;
-      var output = [];
+      //console.log("generateList: " + teamlist);
       for (var i = 0; teams[i] != null; i++) {
-        console.log("generateList: " + teams[i]);
+        //console.log("generateTime: " + teams[i]);
         if (teams[i] == null) {
           break;
         }
-        let val = this.findRowNumber(teams[i], matchList, inital);
-        output.push(val - inital + 1);
+        let val = this.findRowNumber(teams[i].number, matchList, inital);
+        //console.log(val)
+        teams[i].time = val - inital + 1;
       }
-      return output;
+      return teams;
     },
     findRowNumber: function (
       searchString,
       matchList,
       inital
     ) {
-      console.log("findColumnNumber" + searchString);
+      //console.log("findColumnNumber: " + searchString);
       var i = inital - 1;
+      //console.log(this.matchList);
+      //console.log("findColumnNumber: " + this.matchList.length);
+      i = Math.max(1, i);
+      console.log(matchList)
       for (i; i < matchList.length; i++) {
         console.log("findColumnNumber: " + matchList[i]);
         if (
@@ -228,42 +368,48 @@ export const app = {
       }
       return i;
     },
-    updateFunction : function(){
+    updateFunction: function () {
       console.log("updateFunction");
       let eventCode = this.eventCode;
-      if(eventCode === ""){
+      if (eventCode === "") {
         return;
       }
-      if(!eventCode.startsWith("RE-VRC-")){
-        return;
-      }
-      if(eventCode.length != 14){
+      if (!eventCode.startsWith("RE-")) {
         return;
       }
       this.updateSortedTeamList();
       console.log("e");
-      
+
     },
+    reverseTeamList: function () {
+      this.displayTeamList = this.displayTeamList.reverse();
+    }
   },
   mounted() {
+    this.sortedTeamList = { number: "3050X", time: 0, selected: false };
     const searchParams = new URLSearchParams(window.location.search);
     console.log("mounted");
     this.interval = setInterval(() => this.updateFunction(), 60000);
     if (searchParams.has("e")) {
       console.log("Event code found in URL");
       this.eventCode = searchParams.get("e");
-      this.updateSortedTeamList();
+      this.updateAll();
+
       setCookie("eventCode", this.eventCode, 3);
-    }else{
+
+    } else {
       console.log("Event code not found in URL");
       this.eventCode = getCookie("eventCode");
-      if(this.eventCode != ""){
-        this.updateSortedTeamList();
+      if (this.eventCode != "") {
+        this.updateAll();
       }
     }
-    
+    let cookieValue = getCookie('selectedTeamList');
+    if (cookieValue) {
+      this.selectedTeamList = JSON.parse(cookieValue);
+    }
   },
-  beforeDestroy(){
+  beforeDestroy() {
     clearInterval(this.interval);
   }
 
@@ -273,8 +419,8 @@ export const app = {
 function setCookie(cname, cvalue, exdays) {
   console.log("Setting cookie")
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -283,7 +429,7 @@ function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
